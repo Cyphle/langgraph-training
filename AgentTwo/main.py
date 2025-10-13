87f3e2841cc2e2ea85923a782eaffa190a0b8521
Cyphle <cyril.phamle@gmail.com>
2025-10-13 16:57:45 +0200

    Graph of second agent

--- AgentTwo/main.py
+++ AgentTwo/main.py
@@ -1,6 +1,29 @@
 import datetime
+from typing import List
 from dotenv import load_dotenv
 load_dotenv()
+
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph import END, MessageGraph
+from chains import first_responder, revisor
+from tool_executor import execute_tools
+
+MAX_ITERATIONS = 2
+builder = MessageGraph()
+builder.add_node("draft", first_responder)
+builder.add_node("execute_tools", execute_tools)
+builder.add_node("revise", revisor)
+builder.add_edge("draft", "execute_tools")
+builder.add_edge("execute_tools", "revise")
+
+def event_loop(state: List[BaseMessage]) -> str:
+    if len(state) > MAX_ITERATIONS:
+        return END
+    return "draft"
+
+builder.add_conditional_edges("revise", event_loop, {END: END, "execute_tools":"execute_tools"})
+builder.set_entry_point("draft")
+graph = builder.compile()
 
 
 if __name__ == "__main__":
